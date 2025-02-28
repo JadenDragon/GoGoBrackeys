af192a3a9e434b12c48714a83ec35736137e1250
Jogum Cannegieter <joqum.C@gmail.com>
2025-02-28 23:04:13 +0100

    action
    created actions for the controller

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -6,9 +6,11 @@ extends Area2D
 func _on_body_entered(body):
 	pass # Replace with function body.
 	print("You died!")
-	
+	Engine.time_scale = 0.7
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	pass # Replace with function body.
 	get_tree().reload_current_scene()
